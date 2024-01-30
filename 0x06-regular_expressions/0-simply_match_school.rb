4b533f463e7c91d174815fb807d4dcfa4d80610f
DarkWeirdo <sonic_unleashed@live.com>
2024-01-30 22:43:48 +0200

    task 0 fix

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,9 @@
 #!/usr/bin/env ruby
 
-require 'oniguruma'
-
 def match_school(arg)
- regex = Oniguruma::ORegexp.new('School')
- unless arg =~ regex
-    puts "Argument does not match 'School'"
-    exit
- end
+ regex = /School/
+ matches = arg.scan(regex)
+ puts matches.join
 end
 
 match_school(ARGV[0])
