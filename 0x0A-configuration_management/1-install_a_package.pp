8791dee46f057da30e655997cd83ccc53dee71d2
DarkWeirdo <sonic_unleashed@live.com>
2024-03-25 13:21:36 +0200

    task1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,7 @@
+# This Puppet manifest installs Flask version 2.1.0 using pip3.
+
+exec { 'install_flask':
+  command => 'pip3 install Flask==2.1.0',
+  path    => ['/usr/local/bin', '/usr/bin'],
+  unless  => 'pip3 show Flask | grep "Version: 2.1.0"',
+}
