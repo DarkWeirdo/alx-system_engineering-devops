857192ed3b213504102c889d74f0003affed7f3c
DarkWeirdo <sonic_unleashed@live.com>
2024-03-25 13:33:36 +0200

    task1 fix2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,14 @@
-# This Puppet manifest configures the host by creating a file in /tmp and installing Flask version 2.1.0 using pip3.
+# This Puppet manifest installs Flask version 2.1.0 using pip3.
 
-# Create a file in /tmp
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => 'present',
 }
 
-# Install Flask version 2.1.0 using pip3
+# Install Flask version 2.1.0
 exec { 'install_flask':
   command => 'pip3 install Flask==2.1.0',
-  path    => ['/usr/local/bin', '/usr/bin'],
+  path    => '/usr/local/bin:/usr/bin:/bin',
   unless  => 'pip3 show Flask | grep "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
